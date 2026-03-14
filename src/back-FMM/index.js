050e84bfc457417e4194bc350f3a01a2c7bd8913
felmormar <felipe@Fotatch.localdomain>
2026-03-14 09:37:05 +0100

    cambios get post put delete a nedb, hago coleccion y añado la coleccion a la parte script de package.json

--- src/back-FMM/index.js
+++ src/back-FMM/index.js
@@ -1,8 +1,15 @@
-import datosFMM from "../../datos-fmm.json" with { type: "json" };
+//import datosFMM from "../../datos-fmm.json" with { type: "json" };
+
+import Datastore from 'nedb';
+import fs from 'fs';
 let BASE_URL_API = "/api/v1/agriculture-land";
 
-function loadBackendFMM(app) {
+const db = new Datastore({ filename: './src/db/agriculture-land.db', autoload: true });
+const jsonRawData = fs.readFileSync('./datos-fmm.json', 'utf8');
+const initialAgricultureData = JSON.parse(jsonRawData);
 
+function loadBackendFMM(app) {
+/*
 let mediaLandAgriculture =
   datosFMM
     .filter((d) => d.index >= 0 && d.index <= 0.5)
@@ -14,22 +21,92 @@ app.get("/samples/FMM", (req, res) => {
   res.send(
     `<html><body><h1>Media de tierra que es apta para agricultura por indice de pulicion entre 0 y 0.5 :${mediaLandAgriculture}</h1></body></html>`,
   );});
+*/
+
+app.get(BASE_URL_API + "/loadInitialData", (req, res) => {
+    // 1. Borramos todo lo que haya en la DB para no duplicar
+    db.remove({}, { multi: true }, (err, numRemoved) => {
+        if (err) {
+            res.status(500).send("Error limpiando la base de datos");
+        } else {
+            // 2. Insertamos los datos que hemos leído del JSON
+            db.insert(initialAgricultureData, (err, newDocs) => {
+                if (err) {
+                    res.status(500).send("Error insertando datos iniciales");
+                } else {
+                    res.status(201).send(`Cargados ${newDocs.length} recursos del archivo JSON.`);
+                }
+            });
+        }
+    });
+});
 
 
+
+/*
   app.get(BASE_URL_API + "/loadInitialData", async (req, res) => {
   if (datosFMM.length === 0) {
     datosFMM = require("./datos-fmm.json");
     return res.send(layout(`<p>nope</p>`));
   }
   res.send(layout(`<p>Creado con exito</p>`));
-});
+});*/
 //GET ALL
-
+/*
 app.get(BASE_URL_API + "/", (req, res) => {
   res.status(200).send(datosFMM);
 });
+*/
+
+/*
+app.get(BASE_URL_API + "/", (req, res) => {
+    db.find({}, (err, docs) => {
+        docs.forEach(d => delete d._id); // Limpiamos el ID interno de NeDB
+        res.status(200).send(docs);
+    });
+});*/
+
+
+// GET General: Soporta Búsquedas (Punto 3) y Paginación (Punto 4)
+app.get(BASE_URL_API, (req, res) => {
+    // 1. Extraemos todos los posibles parámetros de la URL
+    let { country, year, country_code, land_agriculture, types_land, index, limit, offset } = req.query;
+    
+    // 2. Construimos el objeto de búsqueda (Punto 3)
+    let query = {};
+
+    // Filtros de texto
+    if (country) query.country = country;
+    if (country_code) query.country_code = country_code;
+
+    // Filtros numéricos (usamos Number o parseFloat/parseInt para que coincidan con el JSON)
+    if (year) query.year = parseInt(year);
+    if (land_agriculture) query.land_agriculture = parseFloat(land_agriculture);
+    if (types_land) query.types_land = parseInt(types_land);
+    if (index) query.index = parseFloat(index);
+
+    // 3. Ejecutamos la consulta con Paginación (Punto 4)
+    db.find(query)
+      .skip(parseInt(offset) || 0) 
+      .limit(parseInt(limit) || 0)
+      .exec((err, docs) => {
+          if (err) {
+              res.sendStatus(500);
+          } else {
+              // Limpiamos el campo _id para que la respuesta sea idéntica a tu JSON original
+              const result = docs.map((d) => {
+                  const cleanedDoc = { ...d };
+                  delete cleanedDoc._id;
+                  return cleanedDoc;
+              });
+              res.status(200).send(result);
+          }
+      });
+});
+
 
 //GET
+/*
 app.get(BASE_URL_API + "/:country/:year", (req, res) => {
     const { country, year } = req.params;
 
@@ -60,7 +137,27 @@ app.get(BASE_URL_API + "/:country/:year", (req, res) => {
     res.sendStatus(404); // Not Found
   }
 });
+*/
+app.get(BASE_URL_API + "/:country/:year", (req, res) => {
+    const { country, year } = req.params;
 
+    db.find({ country: country, year: parseInt(year) }, (err, docs) => {
+        if (docs.length > 0) {
+            const resource = docs[0];
+            delete resource._id;
+            res.status(200).json(resource);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
+
+
+
+
+
+////////////////////
+/*
 app.post(BASE_URL_API + "/:country/:year", (req, res) =>
   res.sendStatus(405),
 );
@@ -86,9 +183,30 @@ app.post(BASE_URL_API + "/", (req, res) => {
         datosFMM.push(newData);
         return res.sendStatus(201); // Created
     }
-});
-//////
+}); */
 
+app.post(BASE_URL_API + "/", (req, res) => {
+    let newData = req.body;
+
+    // Validación de campos obligatorios
+    if (!newData || !newData.country || !newData.year) {
+        return res.status(400).send("Bad Request: Faltan campos obligatorios");
+    }
+
+    // Comprobar en la DB si ya existe (usamos find)
+    db.find({ country: newData.country, year: newData.year }, (err, docs) => {
+        if (docs.length > 0) {
+            res.sendStatus(409); // Conflict: El recurso ya existe
+        } else {
+            db.insert(newData, (err, doc) => {
+                res.sendStatus(201); // Created
+            });
+        }
+    });
+});
+
+//////
+ /*
 
 app.put(BASE_URL_API, (req, res) => {
   res.sendStatus(405); // Method Not Allowed
@@ -116,7 +234,26 @@ app.put(BASE_URL_API + "/:country/:country_code", (req, res) => {
     res.sendStatus(404);
   }
 });
+*/
+app.put(BASE_URL_API + "/:country/:year", (req, res) => {
+    const { country, year } = req.params;
+    const updatedData = req.body;
 
+    // Error 400 si los datos del cuerpo no coinciden con la URL
+    if (country !== updatedData.country || parseInt(year) !== updatedData.year) {
+        return res.sendStatus(400);
+    }
+
+    // Actualizamos (upsert: false para que no lo cree si no existe)
+    db.update({ country: country, year: parseInt(year) }, { $set: updatedData }, {}, (err, numReplaced) => {
+        if (numReplaced > 0) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
+/*
 // DELETE
 app.delete(BASE_URL_API + "/", (req, res) => {
   if (req.query.admin !== "true") {
@@ -139,10 +276,29 @@ app.delete(BASE_URL_API + "/:country/:country_code", (req, res) => {
     res.sendStatus(404);
   }
 });
+*/
+app.delete(BASE_URL_API + "/", (req, res) => {
+    // Mantengo tu lógica de admin si la quieres usar
+    if (req.query.admin !== "true") {
+        return res.sendStatus(401); 
+    }
 
+    db.remove({}, { multi: true }, (err, numRemoved) => {
+        res.status(200).send(`Eliminados ${numRemoved} recursos.`);
+    });
+});
 
+app.delete(BASE_URL_API + "/:country/:year", (req, res) => {
+    const { country, year } = req.params;
 
-
+    db.remove({ country: country, year: parseInt(year) }, {}, (err, numRemoved) => {
+        if (numRemoved > 0) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
 
 
 
