ff3412c197fcc466ff196d5dd699d069ad296766
pabgamgar1 <pabgamgar1@alum.us.es>
2026-03-14 18:16:31 +0100

    Move PGG logic to index-PGG.js

--- index-PGG.js
+++ index-PGG.js
@@ -1,115 +1,114 @@
-const datos = [
-  {
-    country: "Afghanistan",
-    code: "AFG",
-    year: 2000,
-    wind: 0,
-    hydro: 0.31,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Afghanistan",
-    code: "AFG",
-    year: 2001,
-    wind: 0,
-    hydro: 0.5,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Albania",
-    code: "ALB",
-    year: 1990,
-    wind: 0,
-    hydro: 2.85,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Algeria",
-    code: "DZA",
-    year: 1965,
-    wind: 0,
-    hydro: 0.4,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "American Samoa",
-    code: "ASM",
-    year: 2000,
-    wind: 0,
-    hydro: 0,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Angola",
-    code: "AGO",
-    year: 2000,
-    wind: 0,
-    hydro: 0.9,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Antigua and Barbuda",
-    code: "ATG",
-    year: 2000,
-    wind: 0,
-    hydro: 0,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Argentina",
-    code: "ARG",
-    year: 1965,
-    wind: 0,
-    hydro: 1.225,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Armenia",
-    code: "ARM",
-    year: 2000,
-    wind: 0,
-    hydro: 1.25,
-    solar: 0,
-    other: 0,
-  },
-  {
-    country: "Aruba",
-    code: "ABW",
-    year: 2000,
-    wind: 0,
-    hydro: 0,
-    solar: 0,
-    other: 0,
-  },
-];
-
-let express = require("express");
+let datosPGG = require("./datos-pgg.json") || [];
 
 let mediaElectricidadHydroAfghanistan =
-  datos
+  datosPGG
     .filter((d) => d.country === "Afghanistan")
     .map((d) => d.hydro)
     .reduce((a, b) => a + b) /
-  datos.filter((d) => d.country === "Afghanistan").length;
+  datosPGG.filter((d) => d.country === "Afghanistan").length;
 
-console.log(mediaElectricidadHydroAfghanistan);
+{
+  let BASE_URL_API = "/api/v1/renewable-energy-consumptions";
 
-const app = express();
+  app.get(BASE_URL_API + "/loadInitialData", async (req, res) => {
+    if (datosPGG.length === 0) {
+      datosPGG = require("./datos-pgg.json");
+      return res.status(201).send("Array was empty: Added data"); // Created
+    }
+    res.status(400).send("Array isn't empty"); // Bad Request
+  });
 
-app.get("/", (req, res) => {
-  res.send(
-    `Media de electridad renovable hidráulica producida por Afghanistan: ${mediaElectricidadHydroAfghanistan}`,
-  );
-});
+  // GET todos
+  app.get(BASE_URL_API + "/", (req, res) => {
+    res.status(200).send(datosPGG);
+  });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000...");
-});
+  // GET dato específico
+  app.get(BASE_URL_API + "/:country/:year", (req, res) => {
+    let { country, year } = req.params;
+    let resource = datosPGG.find(
+      (d) => d.country === country && d.year == year,
+    );
+    if (resource) {
+      res.status(200).send(resource); // OK
+    } else {
+      res.sendStatus(404); // Not Found
+    }
+  });
+
+  // POST (no permitido)
+  app.post(BASE_URL_API + "/:country/:year", (req, res) => res.sendStatus(405));
+
+  // POST (añadir uno nuevo)
+  app.post(BASE_URL_API + "/", (req, res) => {
+    let newData = req.body;
+    // Comprobar campos obligatorios
+    if (!newData.country || newData.year === undefined) {
+      return res.sendStatus(400); // Bad Request
+    }
+    let exists = datosPGG.some(
+      (d) => d.country === newData.country && d.year == newData.year,
+    );
+    if (exists) {
+      res.sendStatus(409); // Conflict
+    } else {
+      datosPGG.push(newData);
+      res.sendStatus(201); // Created
+    }
+  });
+
+  // PUT sobre la lista (NO PERMITIDO)
+  app.put(BASE_URL_API, (req, res) => {
+    res.sendStatus(405); // Method Not Allowed
+  });
+
+  // PUT (sustituir info de uno)
+  app.put(BASE_URL_API + "/:country/:year", (req, res) => {
+    let { country, year } = req.params;
+    let updatedData = req.body;
+
+    // ERROR 400: El país/año de la URL no coincide con el del cuerpo JSON
+    if (country !== updatedData.country || year != updatedData.year) {
+      return res.sendStatus(400);
+    }
+
+    // Posición en el array
+    let index = datosPGG.findIndex(
+      (d) => d.country === country && d.year == year,
+    );
+
+    if (index !== -1) {
+      // Si existe, lo sustituimos por los nuevos datos
+      datosPGG[index] = updatedData;
+      res.sendStatus(200);
+    } else {
+      // ERROR 404: No existe ese país/año para actualizar
+      res.sendStatus(404);
+    }
+  });
+
+  // DELETE todos
+  app.delete(BASE_URL_API + "/", (req, res) => {
+    // Si el usuario no envía .../api/v1/renewable-energy-consumptions?admin=true
+    if (req.query.admin !== "true") {
+      return res.sendStatus(401); // Unauthorized
+    }
+
+    datosPGG = [];
+    res.sendStatus(200);
+  });
+
+  // DELETE dato específico
+  app.delete(BASE_URL_API + "/:country/:year", (req, res) => {
+    let { country, year } = req.params;
+    let index = datosPGG.findIndex(
+      (d) => d.country === country && d.year == year,
+    );
+    if (index !== -1) {
+      datosPGG.splice(index, 1); // Elimina 1 elemento en la posición index
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404); // Not Found
+    }
+  });
+}
